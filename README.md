920b8e80803c4611a6c6995d330816d0f853da4f
Martin Svoboda <martin.svoboda@ixperta.com>
2020-08-06 15:48:24 +0200

    User entity and repository. Used SQLite database (Doctrine).
    README.md updated.

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# phpusersystem
+#  User System in PHP
+
+ - [Installation](#installation)
+	 - [Dependencies](#dependencies)
+	 - [Configuration](#configuration)
+  - [Usage](#usage)
+
+
+User System in PHP is testing project of PHP with framework [Symfony](https://symfony.com/).
+
+## Installation
+
+
+### Dependencies
+Download and install all dependencies:
+ - [Symfony](https://symfony.com/) - use installation instructions at [Symfony Installation Page]([https://symfony.com/doc/current/setup.html](https://symfony.com/doc/current/setup.html)).
+
+### Configuration
+
+
+## Usage
---
+++ src/Entity/User.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=UserRepository::class)
+ */
+class User
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Password;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Rights;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->Name;
+    }
+
+    public function setName(string $Name): self
+    {
+        $this->Name = $Name;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->Password;
+    }
+
+    public function setPassword(string $Password): self
+    {
+        $this->Password = $Password;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->Email;
+    }
+
+    public function setEmail(string $Email): self
+    {
+        $this->Email = $Email;
+
+        return $this;
+    }
+
+    public function getRights(): ?string
+    {
+        return $this->Rights;
+    }
+
+    public function setRights(string $Rights): self
+    {
+        $this->Rights = $Rights;
+
+        return $this;
+    }
+}
